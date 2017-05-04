fc8e790d7dc1180fa934d364b3010ce503dcac72
Sean Allred <code@seanallred.com>
2017-05-04 18:50:32 -0500

    Stop using VERSION

--- ghub+.el
+++ ghub+.el
@@ -36,47 +36,61 @@
     ((repo . "REPO is a repository alist of the form returned by `/user/repos'.")
      (org  . "ORG is an organization alist of the form returned by `/user/orgs'."))
     (lambda (version link) (format "https://developer.github.com/v%d/%s" version link))
-    ghub-get ghub-put ghub-head ghub-post ghub-patch ghub-delete))
+    ghub-get ghub-put ghub-head ghub-post ghub-patch ghub-delete
+    :post-process remove-api-links))
 
 ;;; Utilities
-
 (defmacro ghubp-unpaginate (&rest body)
   "Unpaginate API responses and execute BODY.
 See `ghub-unpaginate'."
   `(let ((ghub-unpaginate t)) ,@body))
 
+(defun remove-api-links (object)
+  "Remove everything in OBJECT that points back to `api.github.com'."
+  ;; execution time overhead of 0.5%
+  (delq nil (if (and (consp object) (consp (car object)))
+                (mapcar #'remove-api-links object)
+              (if (consp object)
+                  (unless (and (stringp (cdr object))
+                               (string-match-p (rx bos (+ alnum) "://api.github.com/")
+                                               (cdr object)))
+                    (cons (car object)
+                          (if (consp (cdr object))
+                              (mapcar #'remove-api-links (cdr object))
+                            (cdr object))))))))
+
 ;;; Repositories
-(ghubp-defget "/repos/:owner/:repo/collaborators"
+(defapiget-ghubp "/repos/:owner/:repo/collaborators"
   "List collaborators."
-  3 "repos/collaborators/#list-collaborators"
+  "repos/collaborators/#list-collaborators"
   repo "/repos/:owner.login/:name/comments")
 
-(ghubp-defget "/repos/:owner/:repo/comments"
+(defapiget-ghubp "/repos/:owner/:repo/comments"
   "List commit comments for a repository."
-  3 "repos/comments/#list-commit-comments-for-a-repository"
+  "repos/comments/#list-commit-comments-for-a-repository"
   repo "/repos/:owner.login/:name/comments")
 
 ;;; Issues
-(ghubp-defget "/issues"
+(defapiget-ghubp "/issues"
   "List all issues assigned to the authenticated user across all
 visible repositories including owned repositories, member
 repositories, and organization repositories."
-  3 "issues/#list-issues")
+  "issues/#list-issues")
 
-(ghubp-defget "/user/issues"
+(defapiget-ghubp "/user/issues"
   "List all issues across owned and member repositories assigned
 to the authenticated user."
-  3 "issues/#list-issues")
+  "issues/#list-issues")
 
-(ghubp-defget "/orgs/:org/issues"
+(defapiget-ghubp "/orgs/:org/issues"
   "List all issues for a given organization assigned to the
 authenticated user."
-  3 "issues/#list-issues"
+  "issues/#list-issues"
   org "/org/:login/issues")
 
-(ghubp-defget "/repos/:owner/:repo/issues"
+(defapiget-ghubp "/repos/:owner/:repo/issues"
   "List issues for a repository."
-  3 "issues/#list-issues-for-a-repository"
+  "issues/#list-issues-for-a-repository"
   repo "/repos/:owner.login/:name/issues")
 
 (provide 'ghub+)
